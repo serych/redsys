8e3bad348ba6c3ee1312e9591065a71dd40515a1
Jakub Šerých <serych@panska.cz>
2024-01-12 10:41:19 +0100

    úvod do PHP

---
+++ index.php
@@ -0,0 +1,24 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<title>Pokus</title>
+</head>
+<body>
+
+<h1>Nadpis</h1>
+<p>Příliš žluťoučký PHP</p>
+<?php
+echo "Ahoj <br>\n";
+echo "Právě je: ";
+?>
+<h1>
+<?php
+echo Date("G:i");
+?>
+</h1>
+</body>
+</html>
+
+
+
+
